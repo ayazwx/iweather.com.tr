4df76f3e3fb13e1b02cc697bb5865abbae2f7016
ayazwx <vefaayazwx@gmail.com>
2024-04-16 16:59:33 +0300

    constants and contexts

--- src/context/DataProvider.tsx
+++ src/context/DataProvider.tsx
@@ -1,24 +1,52 @@
-"use client"
+import React, { createContext, useState } from "react";
+import useLocalStorage from "@/hooks/useLocalStorage";
 import { Location } from "@/types/ValueTypes";
 import { WeatherResponseData } from "@/types/WeatherResponseType";
-import React, { useState } from "react";
 
-export const DataContext = React.createContext({
-  data: undefined as WeatherResponseData | undefined,
-  setData: (data: WeatherResponseData) => {},
-  city: undefined as Location | undefined,
-  setCity: (city: Location) => {},
+export const DataContext = createContext<{
+  data: WeatherResponseData | null;
+  setData: (data: WeatherResponseData | null) => void;
+  city: Location | null;
+  setCity: (city: Location | null) => void;
+  homeCity: Location[] | null;
+  setHomeCity: (city: Location[] | null) => void;
+  addHomeCity: (city: Location) => void;
+}>({
+  data: null,
+  setData: () => {},
+  city: null,
+  setCity: () => {},
+  homeCity: null,
+  setHomeCity: () => {},
+  addHomeCity: () => {},
 });
 
-const DataProvider = ({ children }: { children: React.ReactNode }) => {
-  const [data, setData] = useState<WeatherResponseData>({ "base": "stations", "clouds": { "all": 100 }, "cod": 200, "coord": { "lat": 40.7127, "lon": -74.006 }, "dt": 1711236508, "id": 5128581, "main": { "feels_like": 0.05, "humidity": 86, "pressure": 1006, "temp": 5.5, "temp_max": 7.09, "temp_min": 3.48, "sea_level": 0, "grnd_level" : 0 }, "name": "New York", "sys": { "country": "US", "id": 2008101, "sunrise": 1711191216, "sunset": 1711235470, "type": 2 }, "timezone": -14400, "visibility": 10000, "weather": [{ "description": "overcast clouds", "icon": "04n", "id": 804, "main": "Clouds" }], "wind": { "deg": 340, "gust": 15.43, "speed": 10.8 } });
-  const [city, setCity] = useState<Location>();
+const DataProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [data, setData] = useState<WeatherResponseData | null>(null);
+  const [homeCity, setHomeCity] = useLocalStorage("homeCity");
+  const [city, setCity] = useState<Location | null>(null);
+
+  const addHomeCity = (city: Location) => {
+    if (homeCity) {
+      if (!homeCity.find((home: Location) => home.id === city.id)) {
+        setHomeCity([...homeCity, city]);
+      }
+    } else {
+      setHomeCity([city]);
+    }
+  }
+
 
   const value = {
     data,
     setData,
     city,
     setCity,
+    homeCity,
+    setHomeCity,
+    addHomeCity,
   };
 
   return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
